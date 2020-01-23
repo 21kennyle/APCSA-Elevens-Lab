bfaa1f87117ce2b9a7f6f80a35eeef2d316dd4bf
21kennyle <57225403+21kennyle@users.noreply.github.com>
2020-01-23 09:07:08 -0600

    Update CardTester.java

--- Activity1/CardTester.java
+++ Activity1/CardTester.java
@@ -9,8 +9,8 @@ public class CardTester {
 	 */
 	public static void main(String[] args) {
 Card aceClubs1 = new Card("ace", "clubs", 1);
-Card sixHearts = new Card("6", "hearts", 6);
-Card fiveClubs = new Card("5", "clubs", 5);
+Card threeHearts = new Card("3", "hearts", 3);
+Card twoClubs = new Card("2", "clubs", 2);
 
 
 System.out.println("- ace of clubs Test -");
@@ -20,18 +20,18 @@ System.out.println("  pointValue: " + aceClubs1.pointValue());
 System.out.println("  toString: " + aceClubs1.toString());
 System.out.println();
 
-System.out.println("- six of hearts Test -");
-System.out.println("  rank: " + sixHearts.rank());
-System.out.println("  suit: " + sixHearts.suit());
-System.out.println("  pointValue: " + sixHearts.pointValue());
-System.out.println("  toString: " + sixHearts.toString());
+System.out.println("- three of hearts Test -");
+System.out.println("  rank: " + threeHearts.rank());
+System.out.println("  suit: " + threeHearts.suit());
+System.out.println("  pointValue: " + threeHearts.pointValue());
+System.out.println("  toString: " + threeHearts.toString());
 System.out.println();
 
-System.out.println("- five of clubs Test -");
-System.out.println("  rank: " + fiveClubs.rank());
-System.out.println("  suit: " + fiveClubs.suit());
-System.out.println("  pointValue: " + fiveClubs.pointValue());
-System.out.println("  toString: " + fiveClubs.toString());
+System.out.println("- two of clubs Test -");
+System.out.println("  rank: " + twoClubs.rank());
+System.out.println("  suit: " + twoClubs.suit());
+System.out.println("  pointValue: " + twoClubs.pointValue());
+System.out.println("  toString: " + twoClubs.toString());
 System.out.println();
 
 	}
