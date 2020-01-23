7aa4e48f6def7a453801f60bf25970ab8058d399
21kennyle <57225403+21kennyle@users.noreply.github.com>
2020-01-23 09:40:37 -0600

    Update Card.java

--- Activity1/Card.java
+++ Activity1/Card.java
@@ -3,8 +3,9 @@
  *
  * <code>Card</code> represents a playing card.
  */
-public class Card {
 
+
+public class Card {
 	/**
 	 * String value that holds the suit of the card
 	 */
@@ -32,7 +33,10 @@ public class Card {
 	 *                  containing the point value of the card
 	 */
 	public Card(String cardRank, String cardSuit, int cardPointValue) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		rank = cardRank;
+		suit = cardSuit;
+		pointValue = cardPointValue;
+		
 	}
 
 
@@ -41,7 +45,8 @@ public class Card {
 	 * @return this <code>Card's</code> suit.
 	 */
 	public String suit() {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		return suit;
+		
    }
 
 	/**
@@ -49,7 +54,7 @@ public class Card {
 	 * @return this <code>Card's</code> rank.
 	 */
 	public String rank() {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		return rank;
 	}
 
    /**
@@ -57,7 +62,7 @@ public class Card {
 	 * @return this <code>Card's</code> point value.
 	 */
 	public int pointValue() {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		return pointValue;
 	}
 
 	/** Compare this card with the argument.
@@ -67,7 +72,11 @@ public class Card {
 	 *         false otherwise.
 	 */
 	public boolean matches(Card otherCard) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+	if (suit.equals(otherCard.suit()) && rank.equals(otherCard.rank()) && pointValue == otherCard.pointValue()) {
+        return true;
+	}
+	else 
+		return false;
 	}
 
 	/**
@@ -82,6 +91,8 @@ public class Card {
 	 */
 	@Override
 	public String toString() {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		return rank + " of " + suit + " (point value = " + pointValue + ")";
 	}
+	
+	
 }
