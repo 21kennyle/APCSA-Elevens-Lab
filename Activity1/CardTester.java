e7f403a3c3f224a4097685c54981fe4007aaa887
21kennyle <57225403+21kennyle@users.noreply.github.com>
2020-01-23 08:39:30 -0600

    Update CardTester.java

--- Activity1/CardTester.java
+++ Activity1/CardTester.java
@@ -8,6 +8,31 @@ public class CardTester {
 	 *	@param args is not used.
 	 */
 	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+Card aceClubs1 = new Card("ace", "clubs", 1);
+Card sixHearts = new Card("6", "hearts", 6);
+Card fiveClubs = new Card("5", "clubs", 5);
+
+
+System.out.println("- ace of clubs Test -");
+System.out.println("  rank: " + aceClubs1.rank());
+System.out.println("  suit: " + aceClubs1.suit());
+System.out.println("  pointValue: " + aceClubs1.pointValue());
+System.out.println("  toString: " + aceClubs1.toString());
+System.out.println();
+
+System.out.println("- six of hearts Test -");
+System.out.println("  rank: " + sixHearts.rank());
+System.out.println("  suit: " + sixHearts.suit());
+System.out.println("  pointValue: " + sixHearts.pointValue());
+System.out.println("  toString: " + sixHearts.toString());
+System.out.println();
+
+System.out.println("- five of clubs Test -");
+System.out.println("  rank: " + fiveClubs.rank());
+System.out.println("  suit: " + fiveClubs.suit());
+System.out.println("  pointValue: " + fiveClubs.pointValue());
+System.out.println("  toString: " + fiveClubs.toString());
+System.out.println();
+
 	}
 }
